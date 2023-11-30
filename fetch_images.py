f8478b602619e9c1bf29264488d4bf3d15bde6db
csae8092 <peter.andorfer@oeaw.ac.at>
2023-11-30 17:04:26 +0100

    lower case file names

--- fetch_images.py
+++ fetch_images.py
@@ -7,7 +7,8 @@ os.makedirs(out_dir, exist_ok=True)
 url_stub = (
     "https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/{}&width={}"
 )
-img_list = glob.glob("./bio-pics/*.*")
+img_list = glob.glob("bio-pics/*.*")
+print(img_list)
 
 data = requests.get(
     "https://raw.githubusercontent.com/emt-project/emt-entities/main/json_dumps/persons.json"
@@ -22,10 +23,10 @@ headers = {
 for key, value in data.items():
     emt_id = value["emt_id"]
     img_name = value["img_name"]
-    if img_name:
+    if img_name and img_name.lower().endswith(".jpg"):
         image_url = url_stub.format(img_name, "250")
         img_format = img_name.split(".")[-1]
-        img_name = os.path.join(out_dir, f"{emt_id}.{img_format}")
+        img_name = os.path.join(out_dir, f"{emt_id}.{img_format}".lower())
         if img_name in img_list:
             continue
         r = requests.get(image_url, headers=headers)
